2d2e472a8173f51862d1aae058a17fa5b6f64af7
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-09-13 00:25:52 +0300

    Add the header

---
+++ src/Components/Body.jsx
@@ -0,0 +1,9 @@
+export default function Body() {
+    return (
+        <pre>
+            <span style={{ color: "#f92672", fontSize: "18px" }}>function</span>{" "}
+            <span style={{ color: "#66d9ef", fontSize: "16px" }}>introduce</span>() {"{"}{"\n"}
+            {"    const student = {"}{"\n"}
+        </pre>
+    )
+}
---
+++ src/Components/Top.jsx
@@ -0,0 +1,16 @@
+//Create the header
+export default function top() {
+    return (
+        <header className="top">
+            <p>{"<EvE />"}</p>
+            <div className="navbar">
+                <img src="/Image/Vector 1.svg" />
+                <img src="/Image/github.svg" />
+                <img src="/Image/gmail.svg" />
+                <img src="/Image/linkedin.svg" />
+                <img src="/Image/Vector 1.svg" />
+            </div>
+
+        </header>
+    )
+}
