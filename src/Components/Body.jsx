135f62bc9c7f91b6d68e3eaa742542328de46b6a
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-09-13 01:12:21 +0300

    Add the details for the main

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,9 +1,35 @@
 export default function Body() {
     return (
         <pre>
-            <span style={{ color: "#f92672", fontSize: "18px" }}>function</span>{" "}
-            <span style={{ color: "#66d9ef", fontSize: "16px" }}>introduce</span>() {"{"}{"\n"}
-            {"    const student = {"}{"\n"}
+            <span style={{ color: "#f92672", fontSize: "31.443px" }}>function</span>{" "}
+            <span style={{ color: "#66d9ef", fontSize: "31.443px" }}>introduce</span>() {"{"}{"\n"}
+            <span style={{color: "#FF5C8A", fontSize: "31.443px"}}>{"    const student = {"}{"\n"}</span>
+            <span style={{ color: "#66d9ef", fontSize: "18.531px" }}>               name:</span>{" "}
+            <span style={{ color: "#e6db74", fontSize: "18.531px" }}>"Anh (Evelyn) Nguyen"</span>,{"\n"}
+
+            <span style={{ color: "#66d9ef", fontSize: "18.531px" }}>               role:</span>{" "}
+            <span style={{ color: "#e6db74", fontSize: "18.531px" }}>"Computer Applications Student"</span>,{"\n"}
+
+            <span style={{ color: "#66d9ef", fontSize: "18.531px" }}>               university:</span>{" "}
+            <span style={{ color: "#e6db74", fontSize: "18.531px" }}>"Hameenlinna UAS, Finland"</span>,{"\n"}
+
+            <span style={{ color: "#66d9ef", fontSize: "18.531px" }}>               year:</span>{" "}
+            <span style={{ color: "#fd971f", fontSize: "18.531px" }}>3</span>,{"\n"}
+
+            <span style={{ color: "#66d9ef", fontSize: "18.531px" }}>               backstory:</span>{" "}
+            <span style={{ color: "#e6db74", fontSize: "18.531px" }}>
+                "Started coding with simple web pages → now building full projects that actually work (most of the time)"
+            </span>,{"\n"}
+
+            <span style={{ color: "#66d9ef", fontSize: "18.531px" }}>               fun_fact:</span>{" "}
+            <span style={{ color: "#e6db74", fontSize: "18.531px" }}>
+                "Enjoys ice skating and actually likes the Finnish snow"
+            </span>,{"\n"}
+
+            <span style={{ color: "#66d9ef", fontSize: "18.531px" }}>               looking_for:</span>{" "}
+            <span style={{ color: "#e6db74", fontSize: "18.531px" }}>
+                "Internship starting Aug 2025 (Frontend focus, open to learn backend too)"
+            </span>
         </pre>
     )
 }
