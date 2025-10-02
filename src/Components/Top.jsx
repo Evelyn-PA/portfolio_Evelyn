4ab567a6f3ba74c23f64152419dc8480348f43bc
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-10-02 16:10:51 +0300

    Make more responsive for the site

--- src/Components/Top.jsx
+++ src/Components/Top.jsx
@@ -2,17 +2,19 @@
 export default function top() {
     return (
         <header className="top">
-            <p>{"<EvE />"}</p>
+            <div className="brand">
+                <p>{"<EvE />"}</p>
+            </div>
             <div className="navbar">
                 <img src="/Image/Vector 1.svg" />
                 <a href="https://github.com/Evelyn-PA" target="_blank" rel="noopener noreferrer">
-                    <img src="/Image/github.svg" alt ="My Github"/>
+                    <img src="/Image/github.svg" alt="My Github" />
                 </a>
                 <a href="mailto:evelynng6165@gmail.com">
                     <img src="/Image/gmail.svg" alt="Email me" />
                 </a>
                 <a href="https://www.linkedin.com/in/anh-evelyn-nguyen-a0a227238/" target="_blank" rel="noopener noreferrer">
-                    <img src="/Image/linkedin.svg" alt ="My LinkedIn"/>
+                    <img src="/Image/linkedin.svg" alt="My LinkedIn" />
                 </a>
                 <img src="/Image/Vector 1.svg" />
             </div>
