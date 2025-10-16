e3df62636aba7d202ab80332d001ddaefea0526d
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-10-17 00:12:13 +0300

    Feat: Create Education page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,21 @@
-import Header from "../Components/Top"
 import Body from "../Components/Body"
-
 import Img from "/Image/avt.gif"
 import '../App.css'
 
 export default function Home() {
 
     return (
-        <div className='page'>
-            
+        <div >
+
             <div className="textnimg">
                 <div className="the-code">
                     <Body />
-
-                    
                 </div>
                 <div className="img">
-                    <img src ={Img} alt ="avatar" id = "avt-img"/>
+                    <img src={Img} alt="avatar" id="avt-img" />
                 </div>
             </div>
-            
+
         </div>
     )
 }
