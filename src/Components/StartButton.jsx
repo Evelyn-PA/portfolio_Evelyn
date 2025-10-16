7d641b4b049fd1ff061c56e3c05a29ac8795c59e
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-10-16 14:16:46 +0300

    Feat: Add link for the Curriculum - using <a>

--- src/Components/StartButton.jsx
+++ src/Components/StartButton.jsx
@@ -1,9 +1,12 @@
 import arrow from '/Image/arrow.svg';
-export default function StartButton(){
-    return(
-        <button className="SButton">
-            Curriculum
-            <img src ={arrow} alt="arrow" className='arrow'/>
+export default function StartButton() {
+    return (
+        <a href="/CV/Anh_(Evelyn)_ Nguyen_ CV.pdf"  target='_blank'>
+            <button className="SButton">
+                Curriculum
+                <img src={arrow} alt="arrow" className='arrow' />
+
             </button>
+        </a>
     )
 }
