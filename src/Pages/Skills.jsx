52122d8c87db09a9a8546334b85cc59964dcead1
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-10-25 16:32:17 +0300

    Feat: Add content to Skills tab (Use own content first for testing)

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -1,5 +1,41 @@
-export default function Skills(){
-    return(
-        <h1>Hello</h1>
+export default function Skills() {
+    return (
+        <pre className="container">
+            <span className="keyword">function</span>{" "}
+            <span className="identifier">projects</span>
+            <span className="name">()</span>
+            <span className="bracket">{" {"}</span>{"\n"}
+
+            <div className="containerEdu">
+                <div className='line'>
+                    <img src={Img} alt="line" id="line" />
+                </div>
+                <div>
+                    <div className='edu1'>
+                        {"    "}
+                        <span className="title">Assembly: Endgame!</span> {"\n"}
+                        {"    "}
+                        <span className="tech-stack">React, JavaScript (ES6), HTML, CSS</span>{"\n"}
+                        {"    "}
+                        <span className="string">a React-based word-guessing game that showcases {"\n"}    my skills in state management, component design, {"\n"}    and front-end development using React, JavaScript, HTML, and CSS.
+                        </span>{"\n"}
+                        {"    "}
+                        <span className="github-link">Link <a href="https://github.com/Evelyn-PA/assembly-endgame" target="_blank" id="github-prj">Github</a></span>{"\n"}
+                    </div>
+                    <div className='edu2'>
+                        {"    "}
+                        <span className="title">Chef Claude</span> {"\n"}
+                        {"    "}
+                        <span className="degree">React, JavaScript (ES6+), HTML, CSS, Vite, Google Gemini API</span>{"\n"}
+                        <span className="string">    Developed Chef Claude, an AI-powered recipe assistant
+                            {"\n"}    built with React and the Google Gemini API,
+                            {"\n"}    showcasing my skills in front-end development, API integration,
+                            {"\n"}    and creating responsive, user-friendly web applications. {"\n"}</span>
+                        {"    "}
+                        <span className="github-link">Link <a href="https://github.com/Evelyn-PA/chef_claude" target="_blank" id="github-prj">Github</a></span>{"\n"}
+                    </div>
+                </div>
+            </div>
+        </pre>
     )
 }
